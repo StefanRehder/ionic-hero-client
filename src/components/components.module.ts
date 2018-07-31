dd0e22e3c7fc057e77dae60fa4259247d6fa7b4c
Stefan Rehder <rehder@monsenso.com>
2018-07-31 21:12:43 +0200

    add styling

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CreateHeroFormComponent } from './create-hero-form/create-hero-form';
 import { HeroDetailsComponent } from './hero-details/hero-details';
 import { HeroListElementComponent } from './hero-list-element/hero-list-element';
+
 @NgModule({
-	declarations: [CreateHeroFormComponent,
-    HeroDetailsComponent,
-    HeroListElementComponent],
-	imports: [],
-	exports: [CreateHeroFormComponent,
-    HeroDetailsComponent,
-    HeroListElementComponent]
+	declarations: [
+        CreateHeroFormComponent,
+        HeroDetailsComponent,
+        HeroListElementComponent
+    ],
+    imports: [],
+    exports: [
+        CreateHeroFormComponent,
+        HeroDetailsComponent,
+        HeroListElementComponent
+    ]
 })
 export class ComponentsModule {}
